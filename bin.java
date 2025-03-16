dc8c7e237cbb1bcf4afc26e4c456693dfd228c71
Esha R <esharamesh2006@gmail.com>
2025-03-16 19:16:10 +0530

    Create bin.java

---
+++ bin.java
@@ -0,0 +1,11 @@
+
+//df
+/* er */
+public class bin{
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,8};
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
